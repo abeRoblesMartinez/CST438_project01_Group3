f4de9394caab48e5d609750c594fed342b5f2947
abeRoblesMartinez <aroblesmartinez@csumb.edu>
2022-02-10 21:26:18 -0800

    Added Registration page and modified DAO with findUser functions

--- app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/UserDAO.java
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/UserDAO.java
@@ -1,11 +1,12 @@
 package com.daclink.drew.sp22.cst438_project01_starter;
 
-        import androidx.room.Dao;
-        import androidx.room.Delete;
-        import androidx.room.Insert;
-        import androidx.room.Query;
-        import androidx.room.Update;
-        import java.util.List;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Room;
+import androidx.room.Update;
+import java.util.List;
 
 @Dao
 public interface UserDAO {
@@ -17,4 +18,6 @@ public interface UserDAO {
     void deleteUser(User user);
     @Query("SELECT * FROM user")
     List<User> listUsers();
+    @Query("SELECT * FROM user WHERE username = :username")
+    User findUser(String username);
 }
--- app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/UserRegistrationPageActivity.java
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/UserRegistrationPageActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.room.Room;
+
 
 public class UserRegistrationPageActivity extends AppCompatActivity {
     private EditText username;
@@ -14,10 +16,14 @@ public class UserRegistrationPageActivity extends AppCompatActivity {
     private Button register;
     private Button goBack;
 
+    private UserDAO mUserDAO;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
+
+        getDatabase();
 
         username = findViewById(R.id.userNameRegister);
         password = findViewById(R.id.passwordRegister);
@@ -28,9 +34,28 @@ public class UserRegistrationPageActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String uName = String.valueOf(username.getText());
+                String newPassword = String.valueOf(password.getText());
+                String newVerPassword = String.valueOf(verPassword.getText());
+
+                User user = mUserDAO.findUser(uName);
+                if (user == null){
+                    if (newPassword != newVerPassword){
+                        mUserDAO.insertUser(new User(uName, newPassword));
+                    }
+                } else {
+
+                }
 
             }
         });
 
+    }
+
+    private void getDatabase(){
+        mUserDAO = Room.databaseBuilder(this, UserDb.class, UserDb.DATABASE_NAME)
+                .allowMainThreadQueries()
+                .build()
+                .getPersonDAO();
     }
 }
